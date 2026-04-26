a87135489fe68ac27aa3dfc0ce954a51b58d9c67
Exxon Mecnix <emecnix@gmail.com>
2026-04-26 20:21:16 +0100

    fix: deposit flow

--- Modules/Payment/actions/InitializePayment.php
+++ Modules/Payment/actions/InitializePayment.php
@@ -28,6 +28,7 @@ class InitializePayment
 
         if (
             $existingPayment &&
+
             $existingPayment->status === PaymentStatusEnum::PENDING &&
             ($existingPayment->meta['checkout_url'] ?? null)
         ) {
