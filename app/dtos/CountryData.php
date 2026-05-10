13aed30a350ad183f3af096f815c21ee9a0b0cf4
Exxon Mecnix <emecnix@gmail.com>
2026-05-10 14:27:29 +0100

    fix: CountryData

--- app/dtos/CountryData.php
+++ app/dtos/CountryData.php
@@ -7,6 +7,7 @@ use Spatie\LaravelData\Data;
 class CountryData extends Data
 {
     public function __construct(
+        public int $id,
         public string $name,
         public string $code,
         public string $dial_code,
